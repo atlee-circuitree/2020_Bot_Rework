acd4a8c3f09fb1f266f686841ba4eeab0e27a4d9
Phil Isaacs <32342350+LemonCow@users.noreply.github.com>
2020-01-29 17:40:38 -0500

    added a stopshootMotors command and mapped out a button for it

---
+++ src/main/java/frc/robot/commands/stopshootMotorsCommand.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class shootMotorsCommand extends Command {
+  public shootMotorsCommand() {
+
+  }
+  @Override
+  protected void initialize() {
+
+   }
+
+  @Override
+  protected void execute() {
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
